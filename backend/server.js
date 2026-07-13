b875e354ed51d9d0755e138f2d015a3732509853
Pratham <prathamkhare930@gmail.com>
2026-07-13 11:28:14 +0530

    Connect Mongodb atlas with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Server Working ✅");
-});
 
+const connectDB = require("./database/db");
 const partnerRoutes = require("./routes/partnerRoutes");
 const logger = require("./middleware/logger");
+
+// Connect Database
+connectDB();
 
 app.use(express.json());
 
